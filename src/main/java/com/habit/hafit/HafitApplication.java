28088422cd511bc18d52ffc7df05246626ddc931
wls313 <71283522+wls313@users.noreply.github.com>
2026-01-06 17:39:25 +0900

    feature : default

---
+++ src/main/java/com/habit/hafit/HafitApplication.java
@@ -0,0 +1,13 @@
+package com.habit.hafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HafitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HafitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habit/hafit/HafitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habit.hafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HafitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
